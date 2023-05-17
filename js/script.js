02077bbcbe0abd47bad44878f3148dbf9458e172
andreathepa <andreadepa32@gmail.com>
2023-05-17 17:15:07 +0200

    Exercise done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// 1 - Chiedo all'utente km da percorrere ed età
+// km da percorrere ed età
 
 let kmtodo = parseInt(prompt('Quanti km vuoi percorrere?').trim());
 console.log(kmtodo)
@@ -6,17 +6,41 @@ console.log(kmtodo)
 let age = parseInt(prompt('Quanti anni hai?').trim());
 console.log(age)
 
-// 2 - calcolare il prezzo del biglietto intero
+//variabili
 
-const cost = 0.21
+const price = 0.21
 
-let ticket_price = (kmtodo * cost);
+let ticket_price = (kmtodo * price);
+
+let sconto_young;
+
+let prezzo_young;
+
+let sconto_over;
+
+let prezzo_over;
 
 
+// condizioni
+
+if (age <18) {
+    sconto_young = (ticket_price * 20 / 100);
+    prezzo_young = (ticket_price - sconto_young);
+    tot_young = prezzo_young.toFixed(2)
+    document.getElementById('ticket').innerHTML= tot_young;
 
 
-//4 - se età utente > uguale di 65 applicare sconto del 40%
+} else if (age >= 65) {
+    sconto_over = (ticket_price * 40 / 100);
+    prezzo_over = (ticket_price - sconto_over);
+    tot_over = prezzo_over.toFixed(2)
+    document.getElementById('ticket').innerHTML= tot_over;
 
-//5 - altrimenti utente paga prezzo pieno
 
-//6 - mostrare il prezzo
+} else {
+    tot_ticket = ticket_price.toFixed(2)
+    document.getElementById('ticket').innerHTML= tot_ticket;
+
+}
+
+
